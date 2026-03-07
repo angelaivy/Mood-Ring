d2d00a5ed905746ac324508714b27fe6093ac40f
angelaivy <anjiwilson21@gmail.com>
2026-03-07 11:58:50 -0800

    add data helper functions and add charts for data viz

---
+++ src/components/MoodDataHelpers.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react';
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import db from '../db';
+
+// Get all data from the db. Includes moods, notes, timestamp, id.
+export function GetAllMoodData() {
+  const [moodLogs, setMoodLogs] = useState([])
+  
+  useEffect(() => {
+    const moodLogsQuery = query(collection(db, 'mood-logs'), orderBy('timestamp', 'desc'));
+    const getMoodLogs = onSnapshot(moodLogsQuery, (snapshot) => {
+      const moodLogsArray = [];
+      snapshot.docs.forEach(doc => {
+        moodLogsArray.push({
+         data: doc.data(),
+         id: doc.id
+        });
+      })
+      setMoodLogs(moodLogsArray);
+    });
+
+    return () => getMoodLogs()
+  }, [])
+
+  return moodLogs
+}
+
+// Get only the moods, no notes or timestamp.
+export function GetMoodData() {
+  const [moods, setMoods] = useState([])
+  const moodDataObj = GetAllMoodData();
+  useEffect(() => {
+    let moodsArr = []
+      for (const moodData of moodDataObj) {
+        moodsArr.push(moodData.data.formData.mood);
+      }
+      setMoods(moodsArr);
+  }, [moodDataObj])
+
+  return moods;
+}
+
+// Get counts for each mood.
+export function MoodCount() {
+  const moods = GetMoodData();
+  const [moodCount, setMoodCount] = useState({
+    '😀': 0,
+    '😜': 0,
+    '😐': 0,
+    '😴': 0,
+    '😢': 0,
+    '😕': 0,
+    '😡': 0,
+  })
+
+  useEffect(() => {
+    const newCount = {
+      '😀': 0, '😜': 0, '😐': 0, '😴': 0, '😢': 0, '😕': 0, '😡': 0,
+    };
+    for (const mood of moods) {
+      if (newCount[mood] !== undefined) {
+        newCount[mood]++
+      }
+    }
+    setMoodCount(newCount);
+  }, [moods])
+
+  return moodCount;
+}
+
+// Get counts of moods.
+export function GetMoodCountsOnly() {
+  const counts = MoodCount();
+  return Object.values(counts);
+}
+
+// Get the most frequent mood symbol.
+export function MostFrequentMood() {
+  const count = MoodCount();
+   // Convert count into an array of pairs.
+  const mostFrequentMood = Object.entries(count)
+    .reduce((max, current) => {
+      // Check if the current count is higher than the max count.
+      return current[1] > max[1] ? current : max;
+    });
+
+  return mostFrequentMood[0]
+}
--- src/components/MoodInsights.jsx
+++ src/components/MoodInsights.jsx
@@ -1,82 +1,96 @@
-import { useState, useEffect } from "react";
-import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
-import db from "../db";
-
+import { Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title, 
+  ArcElement, 
+  Tooltip, 
+  Legend } from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
+import { MostFrequentMood, GetMoodCountsOnly, MoodCount } from './MoodDataHelpers';
+ChartJS.register(
+  ArcElement, 
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip, 
+  Legend);
 
 export default function MoodInsights() {
-  const [moods, setMoods] = useState([])
-  const [topMood, setTopMood] = useState('')
-  const moodCount = {
-      '😀': 0,
-      '😜': 0,
-      '😐': 0,
-      '😴': 0,
-      '😢': 0,
-      '😕': 0,
-      '😡': 0,
-    }
-    
-  useEffect(() => {
-    const moodQuery = query(collection(db, 'mood-logs'), orderBy('timestamp', 'desc'));
-    const getMoods = onSnapshot(moodQuery, (snapshot) => {
-      const moodsArray = [];
-      snapshot.docs.forEach(doc => {
-        moodsArray.push(doc.data().formData.mood);
-      })
-      setMoods(moodsArray);
-    });
+  const countForBarChart = MoodCount();
+  // Bar Chart
+  const barChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Mood Frequency',
+      },
+    },
+  };
 
-    () => getMoods()
-  }, [])
-
-  useEffect(() => {
-    for (const mood of moods) {
-      switch(mood) {
-        case '😀':
-          moodCount['😀']++;
-          break;
-        case '😜':
-          moodCount['😜']++;
-          break;
-        case '😐':
-          moodCount['😐']++;
-          break;
-        case '😴':
-          moodCount['😴']++;
-          break;
-        case '😢':
-          moodCount['😢']++;
-          break;
-        case '😕':
-          moodCount['😕']++;
-          break;
-        case '😡':
-          moodCount['😡']++;
-          break;
-        default:
-          return;
+  const barChartLabels = Object.keys(countForBarChart);
+  const moodStyles = {
+    '😀': { background: '#FFD700', label: 'Happy' },
+    '😜': { background: '#FF69B4', label: 'Silly' },
+    '😐': { background: '#A9A9A9', label: 'Neutral' },
+    '😴': { background: '#6495ED', label: 'Tired' },
+    '😢': { background: '#4169E1', label: 'Sad' },
+    '😕': { background: '#DDA0DD', label: 'Confused' },
+    '😡': { background: '#FF4500', label: 'Angry' },
+  }
+  const barChartData = {
+    labels: barChartLabels,
+    datasets: [
+      { 
+        data: Object.values(countForBarChart),
+        backgroundColor: Object.keys(countForBarChart).map(mood => moodStyles[mood].background),
       }
-    }
+    ],
+  };
 
-    // Convert moodCount into an array of pairs.
-    const mostFrequentMood = Object.entries(moodCount)
-      .reduce((max, current) => {
-        // check if the current count is higher than the max count.
-        return current[1] > max[1] ? current : max;
-      });
-    
-    setTopMood(mostFrequentMood[0])
-  }, [moodCount])
+  // Pie Chart
+  const getMoodCountData = GetMoodCountsOnly();
+  const pieData = {
+    labels: ['😀', '😜', '😐', '😴', '😢', '😕', '😡'],
+    datasets: [
+      {
+        label: 'Mood',
+        data: getMoodCountData,
+        backgroundColor: [
+          'rgba(11, 2, 4, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <>
       <h2>Mood Insights</h2>
       <div>
         <h3>Most frequent Mood</h3>
-        {topMood}
+        {<MostFrequentMood />}
+        <Bar options={barChartOptions} data={barChartData} />;
+        <Pie data={pieData} />
       </div>
-     
      </>
-    
   )
 }
