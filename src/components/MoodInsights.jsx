8b1581eefd369465a61ea4c0145556b672857500
angelaivy <anjiwilson21@gmail.com>
2026-03-07 12:58:16 -0800

    add line chart

--- src/components/MoodInsights.jsx
+++ src/components/MoodInsights.jsx
@@ -2,24 +2,28 @@ import { Chart as ChartJS,
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title, 
   ArcElement, 
   Tooltip, 
   Legend } from 'chart.js';
-import { Pie, Bar } from 'react-chartjs-2';
-import { MostFrequentMood, GetMoodCountsOnly, MoodCount } from './MoodDataHelpers';
+import { Pie, Bar, Line } from 'react-chartjs-2';
+import { MostFrequentMood, GetMoodCountsOnly, MoodCount, GetAllMoodData } from './MoodDataHelpers';
 ChartJS.register(
   ArcElement, 
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip, 
   Legend);
 
 export default function MoodInsights() {
-  const countForBarChart = MoodCount();
   // Bar Chart
+  const countForBarChart = MoodCount();
   const barChartOptions = {
     responsive: true,
     plugins: {
@@ -35,13 +39,13 @@ export default function MoodInsights() {
 
   const barChartLabels = Object.keys(countForBarChart);
   const moodStyles = {
-    '😀': { background: '#FFD700', label: 'Happy' },
-    '😜': { background: '#FF69B4', label: 'Silly' },
-    '😐': { background: '#A9A9A9', label: 'Neutral' },
-    '😴': { background: '#6495ED', label: 'Tired' },
-    '😢': { background: '#4169E1', label: 'Sad' },
-    '😕': { background: '#DDA0DD', label: 'Confused' },
-    '😡': { background: '#FF4500', label: 'Angry' },
+    '😀': { background: '#FFD700' },
+    '😜': { background: '#FF69B4' },
+    '😐': { background: '#A9A9A9' },
+    '😴': { background: '#6495ED' },
+    '😢': { background: '#4169E1' },
+    '😕': { background: '#DDA0DD' },
+    '😡': { background: '#FF4500' },
   }
   const barChartData = {
     labels: barChartLabels,
@@ -82,12 +86,54 @@ export default function MoodInsights() {
     ],
   };
 
+  // Line Chart
+  const lineChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+    scales: {
+      y: { // Configuration for the Y-axis
+        ticks: {
+          callback: (value) => {
+            const scoreToMood = {
+              7: '😀', 6: '😜', 5: '😐', 4: '😴', 3: '😕', 2: '😢', 1: '😡'
+            }
+            return scoreToMood[value];
+          }
+        }
+      }
+    }
+  };
+  const moodScore = {
+    '😀': 7, '😜': 6, '😐': 5, '😴': 4, '😕': 3, '😢': 2, '😡': 1
+  }
+  const moodDataObj = GetAllMoodData().slice().reverse();
+  const lineChartData = {
+    labels: moodDataObj.map(entry => 
+      entry.data.timestamp.toDate().toLocaleDateString()
+    ),
+    datasets: [{
+      label: 'Mood Over Time',
+      data: moodDataObj.map(entry => 
+        moodScore[entry.data.formData.mood]
+      ),
+    }]
+  }
+
   return (
     <>
       <h2>Mood Insights</h2>
       <div>
         <h3>Most frequent Mood</h3>
         {<MostFrequentMood />}
+        <Line options={lineChartOptions} data={lineChartData} />;
         <Bar options={barChartOptions} data={barChartData} />;
         <Pie data={pieData} />
       </div>
