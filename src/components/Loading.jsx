fb31839576f0452dffd0150e887544471624cf2d
angelaivy <anjiwilson21@gmail.com>
2026-03-10 07:47:00 -0700

    use public path for static assets

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-      <img style={{width: '10%'}} src='../src/assets/spinner.gif' aria-label='loading'/>
+      <img style={{width: '10%'}} src='/spinner.gif' aria-label='loading'/>
     </div>
   )
 }
